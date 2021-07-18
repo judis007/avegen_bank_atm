05858a29c4de84bab6a1e40599966b2b1fe66ce1
Judis <judismjeevan@gmail.com>
2021-07-19 00:40:57 +0530

    added a flash notice if there is neither current nor savings account'

--- app/controllers/account_transactions_controller.rb
+++ app/controllers/account_transactions_controller.rb
@@ -19,28 +19,34 @@ class AccountTransactionsController < ApplicationController
 
 
         @transaction = AccountTransaction.create!(amount: params[:amount], transaction_type: params[:option], user_account_id: params[:user_account])
-
+        account_name = Account.find(params[:account_id]).name
         account = UserAccount.find_by(user_id: params[:user_account], account_id: params[:account_id])
-        user = account.id
-        current_balance = account.balance
-        transaction = AccountTransaction.select(:amount).where(user_account_id: 1).last
-        transaction_amount = transaction.amount
 
-        if params[:option] == 'withdraw'
-            final_balance = current_balance.to_f - transaction_amount.to_f
-            if transaction_amount > current_balance
-                redirect_to root_path
-                flash[:alert] = "You don't have balance for the withdrawal!"
-            else
+        unless account.nil?
+            user = account.id
+            current_balance = account.balance
+            transaction = AccountTransaction.select(:amount).where(user_account_id: 1).last
+            transaction_amount = transaction.amount
+
+            if params[:option] == 'withdraw'
+                final_balance = current_balance.to_f - transaction_amount.to_f
+                if transaction_amount > current_balance
+                    redirect_to root_path
+                    flash[:alert] = "You don't have balance for the withdrawal!"
+                else
+                    account.update(balance: final_balance)
+                    redirect_to receipt_path(final_balance: final_balance)
+                end
+            elsif params[:option] == 'deposit'
+                final_balance = current_balance.to_f + transaction_amount.to_f
                 account.update(balance: final_balance)
                 redirect_to receipt_path(final_balance: final_balance)
+            elsif params[:option] == 'balance'
+                redirect_to receipt_path(final_balance: current_balance)
             end
-        elsif params[:option] == 'deposit'
-            final_balance = current_balance.to_f + transaction_amount.to_f
-            account.update(balance: final_balance)
-            redirect_to receipt_path(final_balance: final_balance)
-        elsif params[:option] == 'balance'
-            redirect_to receipt_path(final_balance: current_balance)
+        else
+            redirect_to root_path
+            flash[:alert] = "You don't have a #{account_name} account in your bank!"
         end
 
     end
